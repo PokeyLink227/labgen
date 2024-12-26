e021bba40076a5a4d2c503c173be7f5987a6b798
Thomas Rice <thomasrice7@yahoo.com>
2024-12-26 17:20:17 -0500

    split files and added cli support

---
+++ src/image.rs
@@ -0,0 +1,190 @@
+use gif::{Frame, Encoder, Repeat, DisposalMethod};
+use std::{
+    borrow::Cow,
+    fs::File,
+    io::BufWriter,
+    path::Path
+};
+use crate::maze::{
+    Point,
+    Direction,
+    ConnectionStatus,
+    Grid,
+    Vector2,
+};
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct ColorRGB {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl ColorRGB {
+    fn as_bytes(color_array: &[ColorRGB]) -> Vec<u8> {
+        let mut byte_array: Vec<u8> = vec![0; color_array.len() * 3];
+
+        let mut pos = 0;
+        for pixel in color_array {
+            byte_array[pos + 0] = pixel.red;
+            byte_array[pos + 1] = pixel.green;
+            byte_array[pos + 2] = pixel.blue;
+            pos += 3;
+        }
+
+        return byte_array;
+    }
+}
+
+fn get_color(val: f32) -> ColorRGB {
+    ColorRGB {
+        red: ((255 as f32) * (val + 1.0) / 2.0) as u8,
+        green: ((255 as f32) * (val + 1.0) / 2.0) as u8,
+        blue: ((255 as f32) * (val + 1.0) / 2.0) as u8,
+    }
+}
+
+pub fn generate_gif(maze: &Grid, history: &[(Point, Direction)]) {
+    let passage_width: u16 = 1;
+    let wall_width: u16 = 4;
+    let cell_width: u16 = passage_width + wall_width;
+
+    let color_map = &[0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF];
+    let (width, height) = (maze.width as u16 * cell_width + wall_width, maze.height as u16 * cell_width + wall_width);
+
+    let empty_maze: Vec<u8> = vec![0; width as usize * height as usize];
+    let connected_cell: Vec<u8> = vec![1; (cell_width * cell_width) as usize];
+
+    let mut image = File::create("./animation.gif").unwrap();
+    let mut encoder = Encoder::new(&mut image, width, height, color_map).unwrap();
+    encoder.set_repeat(Repeat::Infinite).unwrap();
+
+    // initial frame to set background
+    let mut frame = Frame::default();
+    frame.width = width;
+    frame.height = height;
+    frame.delay = 0;
+    frame.buffer = Cow::Borrowed(&empty_maze);
+    encoder.write_frame(&frame).unwrap();
+
+    for (pt, dir) in history {
+        let mut frame = Frame::default();
+        frame.delay = 20;
+
+        // set dimensions and position of frame
+        match dir {
+            Direction::None => {
+                frame.width = passage_width;
+                frame.height = passage_width;
+                frame.top = pt.y as u16 * cell_width + wall_width;
+                frame.left = pt.x as u16 * cell_width + wall_width;
+            }
+            Direction::North => {
+                frame.width = passage_width;
+                frame.height = cell_width;
+                frame.top = pt.y as u16 * cell_width + 0;
+                frame.left = pt.x as u16 * cell_width + wall_width;
+            }
+            Direction::East => {
+                frame.width = cell_width;
+                frame.height = passage_width;
+                frame.top = pt.y as u16 * cell_width + wall_width;
+                frame.left = pt.x as u16 * cell_width + wall_width;
+            }
+            Direction::South => {
+                frame.width = passage_width;
+                frame.height = cell_width;
+                frame.top = pt.y as u16 * cell_width + wall_width;
+                frame.left = pt.x as u16 * cell_width + wall_width;
+            }
+            Direction::West => {
+                frame.width = cell_width;
+                frame.height = passage_width;
+                frame.top = pt.y as u16 * cell_width + wall_width;
+                frame.left = pt.x as u16 * cell_width + 0;
+            }
+        }
+
+        frame.buffer = Cow::Borrowed(&connected_cell);
+        frame.dispose = DisposalMethod::Keep;
+        encoder.write_frame(&frame).unwrap();
+    }
+
+    // final empty frame with a higher delay
+    let mut frame = Frame::default();
+    frame.width = 1;
+    frame.height = 1;
+    frame.dispose = DisposalMethod::Keep;
+    frame.delay = 100;
+    frame.buffer = Cow::Borrowed(&[0]);
+    encoder.write_frame(&frame).unwrap();
+}
+
+pub fn generate_png(maze: &Grid) {
+    let cell_width = 5;
+    let image_dimensions = Vector2 {
+        x: maze.width * cell_width + 1,
+        y: maze.height * cell_width + 1,
+    };
+
+    let path = Path::new(r"./image.png");
+    let file = File::create(path).unwrap();
+    let ref mut w = BufWriter::new(file);
+
+    let mut encoder = png::Encoder::new(w, image_dimensions.x, image_dimensions.y);
+    encoder.set_color(png::ColorType::Rgb);
+
+    let mut writer = encoder.write_header().unwrap();
+
+    let mut pixels: Vec<ColorRGB> = vec![
+        ColorRGB {
+            red: 0,
+            green: 0,
+            blue: 0
+        };
+        (image_dimensions.x * image_dimensions.y) as usize
+    ];
+
+    for y in 0..maze.height {
+        for x in 0..maze.width {
+            pixels[((x * cell_width + 1) + ((y * cell_width + 1) * image_dimensions.x)) as usize] =
+                ColorRGB {
+                    red: 255,
+                    green: 255,
+                    blue: 255,
+                };
+            if maze
+                .get_tile(Point {
+                    x: x as i16,
+                    y: y as i16,
+                })
+                .connections & Direction::North as u8 != 0
+            {
+                pixels[((x * cell_width + 1) + ((y * cell_width + 0) * image_dimensions.x))
+                    as usize] = ColorRGB {
+                    red: 255,
+                    green: 255,
+                    blue: 255,
+                };
+            }
+            if maze
+                .get_tile(Point {
+                    x: x as i16,
+                    y: y as i16,
+                })
+                .connections & Direction::West as u8 != 0
+            {
+                pixels[((x * cell_width + 0) + ((y * cell_width + 1) * image_dimensions.x))
+                    as usize] = ColorRGB {
+                    red: 255,
+                    green: 255,
+                    blue: 255,
+                };
+            }
+        }
+    }
+
+    writer
+        .write_image_data(&ColorRGB::as_bytes(&pixels))
+        .unwrap();
+}
